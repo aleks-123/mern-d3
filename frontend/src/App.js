94d6d8783adfeaa95254e23b4928f54b0e41e3c0
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2022-12-27 22:44:34 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
+import DoctorMonitor from "./Components/Doctor Monitor/DoctorMonitor";
+
+function App() {
+  return (
+    <div className="App">
+      <DoctorMonitor />
+    </div>
+  );
+}
+
+export default App;
